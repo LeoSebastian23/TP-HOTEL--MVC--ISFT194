0eb4df823b657f90205a1434a935a2d59af8c179
Leonardo Sebastian Gauto <112297703+LeoSebastian23@users.noreply.github.com>
2024-10-24 02:40:07 -0300

    update

--- src/main/java/Views/PasajerosView.java
+++ src/main/java/Views/PasajerosView.java
@@ -3,10 +3,10 @@ package Views;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
 import Models.PasajeroModel;
 import Controllers.PasajeroController;
-
-import static java.lang.Integer.parseInt;
+import java.util.List;
 
 public class PasajerosView extends JFrame {
     private JTextField dniField;
@@ -16,41 +16,47 @@ public class PasajerosView extends JFrame {
     private JTextField mailField;
     private JButton agregarPasajero;
     private JPanel panelPasajero;
-    private JList list1;
+    private JTable pasajerosTable;
     private JButton modificarButton;
     private JButton eliminarButton;
 
     private final PasajeroController pasajeroController;
+    private DefaultTableModel tableModel;
 
     public PasajerosView(PasajeroController controller) {
         this.pasajeroController = controller;
         setContentPane(panelPasajero);
         setTitle("Gestión de Pasajeros");
-        setSize(400, 300);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        // Configuración de la tabla
+        tableModel = new DefaultTableModel(new Object[]{"ID", "DNI", "Nombre", "Apellido", "Cel", "Mail"}, 0);
+        pasajerosTable.setModel(tableModel);
+        loadPasajerosTable();
 
         agregarPasajero.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    int dni = parseInt(dniField.getText());
+                    int dni = Integer.parseInt(dniField.getText());
                     String name = nameField.getText();
                     String surname = surnameField.getText();
                     String cel = celField.getText();
                     String mail = mailField.getText();
 
-                    PasajeroModel pasajero = new PasajeroModel(4, dni, name, surname, cel, mail);
+                    PasajeroModel pasajero = new PasajeroModel(0, dni, name, surname, cel, mail);
                     pasajeroController.addPasajero(pasajero);
 
                     JOptionPane.showMessageDialog(null, "Pasajero agregado correctamente!");
                     clearFields();
+                    loadPasajerosTable();
                 } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(null, "Por favor, ingresa valores válidos.");
                 }
             }
         });
-
     }
 
     private void clearFields() {
@@ -60,7 +66,21 @@ public class PasajerosView extends JFrame {
         celField.setText("");
         mailField.setText("");
     }
+
+    private void loadPasajerosTable() {
+        // Limpia la tabla antes de agregar nuevos datos
+        tableModel.setRowCount(0);
+
+        List<PasajeroModel> pasajeros = pasajeroController.getAllPasajeros();
+        for (PasajeroModel p : pasajeros) {
+            tableModel.addRow(new Object[]{p.getId(), p.getDni(), p.getName(), p.getSurname(), p.getCel(), p.getMail()});
+        }
+    }
 }
+
+
+
+
 
 
 
